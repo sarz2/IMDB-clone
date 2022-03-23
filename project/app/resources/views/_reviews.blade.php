c4fbb53f37f90bbc148c2be34e7dc33a84e624a5
sarz2 <sara.abou@chasacademy.se>
2022-03-23 18:52:46 +0100

    fix design for button in review form

--- project/app/resources/views/_reviews.blade.php
+++ project/app/resources/views/_reviews.blade.php
@@ -127,40 +127,46 @@
                                             class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
                                             <button type="button"
                                                 class="px-6
-                              py-2.5
-                              bg-purple-600
-                              text-white
-                              font-medium
-                              text-xs
-                              leading-tight
-                              uppercase
-                              rounded
-                              shadow-md
-                              hover:bg-purple-700 hover:shadow-lg
-                              focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-                              active:bg-purple-800 active:shadow-lg
-                              transition
-                              duration-150
-                              ease-in-out"
-                                                data-bs-dismiss="modal">Close</button>
+                                                    mb-2
+                                                    py-2.5
+                                                    bg-purple-600
+                                                    text-white
+                                                    font-medium
+                                                    text-xs
+                                                    leading-tight
+                                                    uppercase
+                                                    rounded
+                                                    shadow-md
+                                                    hover:bg-purple-700 hover:shadow-lg
+                                                    focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
+                                                    active:bg-purple-800 active:shadow-lg
+                                                    transition
+                                                    duration-150
+                                                    ease-in-out"
+                                                data-bs-dismiss="modal">
+                                                Close
+                                            </button>
                                             <button type="submit"
                                                 class="px-6
-                          py-2.5
-                          bg-blue-600
-                          text-white
-                          font-medium
-                          text-xs
-                          leading-tight
-                          uppercase
-                          rounded
-                          shadow-md
-                          hover:bg-blue-700 hover:shadow-lg
-                          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                          active:bg-blue-800 active:shadow-lg
-                          transition
-                          duration-150
-                          ease-in-out
-                          ml-1">Submit</button>
+                                                        mb-2
+                                                        py-2.5
+                                                        bg-blue-600
+                                                        text-white
+                                                        font-medium
+                                                        text-xs
+                                                        leading-tight
+                                                        uppercase
+                                                        rounded
+                                                        shadow-md
+                                                        hover:bg-blue-700 hover:shadow-lg
+                                                        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                                                        active:bg-blue-800 active:shadow-lg
+                                                        transition
+                                                        duration-150
+                                                        ease-in-out
+                                                        ml-1">
+                                                        Submit
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
